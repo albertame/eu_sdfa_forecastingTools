fd918f24662c27d3c7633aa249796f73defd3b0b
albertame <americoalberto1@gmail.com>
2024-06-12 10:00:40 +0200

    refactoring

--- utils.py
+++ utils.py
@@ -24,6 +24,47 @@ def fx_volatility(df, window, volatility_variable):
     df[volatility_variable+"_std"] =  df[volatility_variable].rolling(window).std()*(window**0.5)
     return df
 
+def add_systemic_risk_dummy(data_file, dummy_file, country):
+    df = pd.read_csv(data_file)
+    df['date'] = pd.PeriodIndex(df['date'], freq= 'Q').to_timestamp()
 
+    dummy = pd.read_csv(dummy_file)
+    dummy_de = dummy[dummy['iso2']==country]
+    dummy_de['date'] = pd.to_datetime(dummy_de['date.1'])
+
+    dummy_cc = dummy[dummy['iso2']==country]
+    dummy_cc = dummy_cc.sort_values(by='date.1')
+    dummy_cc.index = pd.to_datetime(dummy_cc['date.1'])
+
+    df = pd.merge(df, dummy_de, on = ['iso2', 'date'], how= 'left')
+
+    df = df.drop(['iso2','date.1_x','month','date.1_y','financialStressDummy'], axis=1)
+    df['is_systemic_crisis'] = df['is_systemic_crisis'].fillna(0)
+    df = df.dropna(axis=1, how = 'all')
+    return df
+
+def add_missing_variables(df):
+    data_ea = pd.read_csv('./data/data_input_quarterly.csv')
+    cols2add = ['date','policyRate', 'EAtermSpread']
+    data_ea = data_ea[cols2add][data_ea['iso2']=='EA']
+    data_ea['date'] = pd.PeriodIndex(data_ea['date'], freq= 'Q').to_timestamp()
+
+    data_us = pd.read_csv('./data/data_input_quarterly.csv')
+    cols2add = ['date','policyRate', 'UStermSpread']
+    data_us = data_us[cols2add][data_us['iso2']=='US']
+    data_us['date'] = pd.PeriodIndex(data_us['date'], freq= 'Q').to_timestamp()
+
+    df.index = df['date']
+    data_ea.index = data_ea['date']
+    data_us.index = data_us['date']
+
+    df['policyRate'] = df['policyRate'].fillna(data_ea['policyRate'])
+    df['EAtermspread'] = data_ea['EAtermSpread']
+    df['USpolicyRate'] = data_us['policyRate']
+    df['UStermSpread'] = data_us['UStermSpread']
+
+    df.index = df['date']
+    df.drop('date', axis=1, inplace = True)
+    return df
 
 
